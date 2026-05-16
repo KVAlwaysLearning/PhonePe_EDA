c8447d5878dd65abb4e4be4914b4d725e12d619e
KVAlwaysLearning <codingacc979@gmail.com>
2026-05-16 21:08:45 +0530

    Update app3.py
    Signed-off-by: KVAlwaysLearning <codingacc979@gmail.com>

--- app3.py
+++ app3.py
@@ -42,7 +42,7 @@ def load_all_pulse_tables():
 dfs = load_all_pulse_tables()
 
 # --- GLOBAL FILTER SIDEBAR CONTROL ---
-st.sidebar.image("https://github.com/KVAlwaysLearning/PhonePe_EDA/blob/main/PhonePe_logo.png", width=160)
+st.sidebar.image("https://raw.githubusercontent.com/KVAlwaysLearning/PhonePe_EDA/main/PhonePe_logo.png", width=160)
 st.sidebar.markdown("### Custom Dataset Subset Filtering")
 
 # 1. Multi-Select for Years
